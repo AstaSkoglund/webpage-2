4e4ebc456700d28a20222e2f8a03d4d354e62bf7
Asta Skoglund <astaskoglund@seattleacademy.org>
2024-01-16 11:41:19 -0800

    done

---
+++ script.js
@@ -0,0 +1,26 @@
+const numberBox = document.getElementById("guess")
+const feedbackBox = document.getElementById("feedback")
+
+const newNumber = Math.ceil(Math.random()*100)
+
+numberBox.addEventListener("change", checkNumber)
+
+function checkNumber(){ 
+    const guess = numberBox.value;
+
+    if (guess < newNumber) {
+
+        feedbackBox.innerText = "Too Low!";
+    }
+    else if (guess > newNumber) {
+
+        feedbackBox.innerText = "Too High!";
+
+    }
+    else{
+
+        feedbackBox.innerText = "just right!";
+    }
+
+
+}
